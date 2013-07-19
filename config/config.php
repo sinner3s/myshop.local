b33c88ebac8e4ffc4a132fb0cdea98d300fa18be
nilras <sinner.wow.ua@gmail.com>
2013-07-19 17:25:32 +0300

    add3

--- config/config.php
+++ config/config.php
@@ -3,3 +3,27 @@
 // Константы для обращение к контроллерам
 define ('PathPrefix', '../controllers/');
 define ('PathPostfix', 'Controller.php');
+
+// используемый шаблон
+
+$template = 'default';
+
+// пути к файлам шаблонов
+define ('TemlatePrefix', "../views/{$template}/");
+define ('TemplatePostfix', '.tpl');
+
+// пути к файлам шаблонов в папке www
+define ('TemplateWebPath', "/templates/{$template}/");
+
+// Инициализация шаблонизатора Smarty
+// full path to Smarty.class.php
+require ('../library/Smarty/libs/Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->setTemplateDir(TemlatePrefix);
+$smarty->setCompileDir('../tmp/smarty/templates_c');
+$smarty->setCacheDir('../tmp/smarty/cache');
+$smarty->setConfigDir('../library/Smarty/configs');
+
+$smarty->assign('templateWebPath', TemplateWebPath);
+
--- library/mainFunctions.php
+++ library/mainFunctions.php
@@ -1,8 +1,8 @@
 <?php
 
-function loadPage($controllerName, $actionName = 'index'){
+function loadPage($smarty, $controllerName, $actionName = 'index'){
     include_once PathPrefix . $controllerName . PathPostfix;
     
     $function = $actionName . 'Action';
-    $function();
+    $function($smarty);
 }
